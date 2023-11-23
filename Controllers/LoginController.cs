72029ba7f21d31bae1fe70b1b2185142625e4c53
rafifiaan <rafialiefian03@gmail.com>
2023-11-23 12:43:33 +0700

    build(release): project src

---
+++ Controllers/LoginController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace GenshinCourse.Controllers
+{
+    public class LoginController : Controller
+    {
+        // GET: Login
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+
+        public ActionResult Index(user objCheck)
+        {
+            if (ModelState.IsValid)
+            {
+                using (GenshinCourse_dbEntities db = new GenshinCourse_dbEntities())
+                {
+                    var obj = db.users.Where(a => a.uname.Equals(objCheck.uname) && a.password.Equals(objCheck.password)).FirstOrDefault();
+
+                    if (obj != null)
+                    {
+                        Session["UserID"] = obj.id.ToString();
+                        Session["UserName"] = obj.uname.ToString();
+                        return RedirectToAction("Index", "Home");
+                    }
+                    else
+                    {
+                        ModelState.AddModelError("", "The UserName or Password Incorrect");
+                    }
+                }
+            }
+            return View(objCheck);
+        }
+        public ActionResult Logout()
+        {
+            Session.Clear();
+            return RedirectToAction("Index", "Home");
+
+        }
+    }
+}
